95b0401b7e704b97d7162cf07efc33377ee9d69c
Manuel Gonçalves <manuel_11_94@hotmail.es>
2019-07-02 15:10:25 -0400

    bundle config

--- WebStore/App_Start/BundleConfig.cs
+++ WebStore/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace WebStore
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.min.js",
+                        "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery.steps.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
